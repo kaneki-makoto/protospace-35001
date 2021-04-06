245d95e70b0117bd00bd3b7ba7f8e240d914da7c
kaneki-makoto <kanakimakoto@kanagimakotonoMacBook-Pro.local>
2021-04-06 11:49:32 +0900

    モデル作成、カラム追加、prototypeとuserモデルにアソシエーションを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_may :prototypes
+
   validates :name, :profile, :occupation, :position, presence: true
 end
